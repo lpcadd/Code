139b1b90a2d01f532a8debef2241571981e4a770
LI PENG <peterlee@live.com>
2020-05-02 14:05:01 +0800

    Add files via upload

---
+++ python/HF2.py
@@ -0,0 +1,15 @@
+file = open('./a.txt','r')
+
+readline = file.readlines()
+
+i = 1
+for line in readline:
+    i = i + 1
+    if i%2==0:
+        new_line = line.split('HF=')[1].split('\R')[0]
+        new_file = open('./b.txt','a')
+        new_file.write(new_line+'\n')
+        
+       		
+	
+print(new_line)
